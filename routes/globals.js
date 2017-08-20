fad0b79cc29cb2f205f772f9536244fae356a2aa
Fabio Albertin <albertin@gmail.com>
2017-08-20 23:46:21 +0100

    Complete up to PATCH

--- routes/globals.js
+++ routes/globals.js
@@ -41,6 +41,68 @@ exports.register = (server, options, next) => {
     }
   });
 
+  server.route({
+    method: 'POST',
+    path: '/globals'
+    handler: (req, res) => {
+      const global = req.payload;
+
+      book._id = uuid.v1();
+
+      db.globals.save(global, (err, result) => {
+        if (err) {
+          return res(Boom.wrap(err, 'Internal MongoDB Error'));
+        }
+
+        res(global);
+      });
+    },
+    config: {
+      validate: {
+        payload: {
+          name: Joi.string().min(2).max(50).required(),
+          value: Joi.string().min(1).max(50),
+          category: Joi.string().min(4).max(50).required(),
+          author: Joi.string().min(3).max(50).required(),
+          rank: Joi.number()
+        }
+      }
+    }
+  });
+
+  server.route({
+    method: 'PATCH',
+    path: '/globals/{id}',
+    handler: (req, res) => {
+      db.globals.update({
+        _id: req.params.id
+      }, {
+        $set: req.payload
+      }, (err, res) => {
+        if (err) {
+          return res(Boom.wrap(err, 'Internal MongoDB Error'));
+        }
+
+        if(res.n === 0) {
+          return res(Boom.notFound());
+        }
+
+        res().code(204);
+      });
+    },
+    config: {
+      validate: {
+        payload: Joi.object({
+          name: Joi.string().min(2).max(50).optional(),
+          value: Joi.string().min(1).max(50).optional(),
+          category: Joi.string().min(4).max(50).optional(),
+          author: Joi.string().min(3).max(50).optional(),
+          rank: Joi.number().optional()
+        }).required().min(1)
+      }
+    }
+  });
+
   return next();
 };
 
